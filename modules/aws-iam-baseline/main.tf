c4a6bd339a81bb198ba6b222beb5416132774885
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-17 18:25:02 +0100

    Update main.tf

--- modules/aws-iam-baseline/main.tf
+++ modules/aws-iam-baseline/main.tf
@@ -28,3 +28,30 @@ resource "aws_iam_role_policy_attachment" "admin_policy" {
   role       = aws_iam_role.break_glass_admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+########################################
+# Read-Only Role
+########################################
+
+resource "aws_iam_role" "read_only" {
+  name = "${var.environment}-read-only"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.read_only_principal_arns
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "read_only_policy" {
+  role       = aws_iam_role.read_only.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
