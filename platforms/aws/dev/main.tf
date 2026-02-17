c7734d871f94f4d9e1c486408bc4daebafedab1e
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-17 18:20:23 +0100

    Update main.tf

--- platforms/aws/dev/main.tf
+++ platforms/aws/dev/main.tf
@@ -1,3 +1,13 @@
 locals {
   project_name = "multicloud-platform-baseline"
 }
+
+module "network" {
+  source = "../../../modules/aws-network"
+
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.10.0/24", "10.0.20.0/24"]
+  availability_zones   = ["eu-central-1a", "eu-central-1b"]
+  environment          = var.environment
+}
