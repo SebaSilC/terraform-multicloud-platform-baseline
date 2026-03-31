42caec2d63bde7895a59bb6197881766c132ae64
SebaSilC <sebastian@playbookvisualarts.com>
2026-03-31 17:25:06 +0200

    Modify Terraform version and AWS provider version
    Updated required version constraints for Terraform and AWS provider.

--- platforms/aws/dev/versions.tf
+++ platforms/aws/dev/versions.tf
@@ -1,18 +1,12 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.6.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.30"
     }
   }
 
-  backend "s3" {
-    bucket         = "tf-state-platform-baseline-<your-suffix>"
-    key            = "aws/dev/platform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
